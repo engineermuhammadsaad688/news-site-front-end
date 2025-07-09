82a8c6961f7ef054e5a4344cbe74b3696fc85d05
muhammad saad <engineer.muhammadsaad688@gmail.com>
2025-07-09 13:05:11 +0500

    code-rmove signup

--- src/components/signUp/signUPMain.js
+++ src/components/signUp/signUPMain.js
@@ -1,49 +1,13 @@
-import { useNavigate } from 'react-router-dom';
 import './signUPMain.css';
-import { postData } from '../../services/api';
-import { useState } from 'react';
 
 
 function SignUPMain() {
-    const navigate = useNavigate();
 
 
 
-    const [formData, setFormData] = useState({
-        email: '',
-        password: '',
-        name: ''
-    });
-
-
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-    };
-
-
-
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            // Dummy API call
-            const response = await postData('/users/signUP', formData);
-            console.log('Login successful:', response.data);
-
-            // Redirect to category 
-            navigate('/category');
-        } catch (error) {
-            console.error('Login failed:', error);
-        }
-    };
     return (
         <div className='signup-container'>
-            <form onSubmit={handleSubmit} className='login-form'>
+
 
                 <div className='signup-box'>
 
@@ -60,8 +24,6 @@ function SignUPMain() {
                                 type="name"
                                 name="name"
                                 placeholder="Enter your name"
-                                value={formData.username}
-                                onChange={handleChange}
                                 required
                                 className="input-field"
                             />
@@ -75,8 +37,6 @@ function SignUPMain() {
                                 type="email"
                                 name="email"
                                 placeholder="Enter your email"
-                                value={formData.username}
-                                onChange={handleChange}
                                 required
                                 className="input-field"
                             />
@@ -90,8 +50,6 @@ function SignUPMain() {
                                 type="password"
                                 name="password"
                                 placeholder="Enter your password"
-                                value={formData.username}
-                                onChange={handleChange}
                                 required
                                 className="input-field"
                             />
@@ -104,7 +62,6 @@ function SignUPMain() {
                         </div>
                     </div>
                 </div>
-            </form>
         </div>
     );
 }
